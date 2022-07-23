0b74650e0ae082adbc9dc4e9d21b467beea8cbfc
louissxu <louis.s.xu@gmail.com>
2022-07-23 18:43:58 +0930

    feat: Add menu buttons and ability to step forward and backward through moves

--- src/Visualiser.tsx
+++ src/Visualiser.tsx
@@ -104,6 +104,41 @@ class Moves extends React.Component<IMovesProps, IMovesState> {
   }
 }
 
+interface IMenuProps {
+  stepForward: React.MouseEventHandler<HTMLButtonElement>
+  stepBackward: React.MouseEventHandler<HTMLButtonElement>
+}
+
+interface IMenuState {
+
+}
+
+class Menu extends React.Component<IMenuProps, IMenuState> {
+  constructor(props: IMenuProps) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.props.stepForward}
+        >
+          Step Forward
+        </button>
+        <button
+          onClick={this.props.stepBackward}
+        >
+          StepBackward
+        </button>
+      </div>
+    )
+  }
+}
+
 interface IVisualiserProps {
   values: Array<number>,
   moves: Array<Move>,
@@ -113,21 +148,47 @@ interface IVisualiserState {
   stack_a: Array<number>,
   stack_b: Array<number>,
   moves: Array<Move>,
+  skipped_moves: Array<Boolean>,
   next_move_num: number,
 }
 
 class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
   constructor(props: IVisualiserProps) {
     super(props)
+    const skipped_moves: Array<boolean> = []
+    let left_length = this.props.values.length;
+    let right_length = 0;
+    for (let i = 0; i < this.props.moves.length; i++) {
+      if (this.props.moves[i] === Move.Pa) {
+        if (right_length > 0) {
+          right_length--;
+          left_length++;
+          skipped_moves.push(false);
+        } else {
+          skipped_moves.push(true);
+        }
+      } else if (this.props.moves[i] === Move.Pb) {
+        if (left_length > 0) {
+          left_length--;
+          right_length++;
+          skipped_moves.push(false);
+        } else {
+          skipped_moves.push(true);
+        }
+      } else {
+        skipped_moves.push(false);
+      }
+    }
     this.state = {
       stack_a: this.props.values,
       stack_b: [],
       moves: this.props.moves,
+      skipped_moves: skipped_moves,
       next_move_num: 0,
     }
   }
   
-  psPrimitiveSa () {
+  psPrimitiveSa() {
     if (this.state.stack_a.length <= 1) {
       return;
     }
@@ -140,7 +201,7 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveSb () {
+  psPrimitiveSb() {
     if (this.state.stack_b.length <= 1) {
       return;
     }
@@ -153,12 +214,12 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveSs () {
+  psPrimitiveSs() {
     this.psPrimitiveSa();
     this.psPrimitiveSb();
   }
 
-  psPrimitivePa () {
+  psPrimitivePa() {
     if (this.state.stack_b.length <= 0) {
       return;
     }
@@ -172,7 +233,7 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitivePb () {
+  psPrimitivePb() {
     if (this.state.stack_a.length <= 0) {
       return;
     }
@@ -186,7 +247,7 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveRa () {
+  psPrimitiveRa() {
     if (this.state.stack_a.length <= 0) {
       return;
     }
@@ -198,7 +259,7 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveRb () {
+  psPrimitiveRb() {
     if (this.state.stack_a.length <= 0) {
       return;
     }
@@ -210,12 +271,12 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }  
 
-  psPrimitiveRr () {
+  psPrimitiveRr() {
     this.psPrimitiveRa();
     this.psPrimitiveRb();
   }
 
-  psPrimitiveRra () {
+  psPrimitiveRra() {
     if (this.state.stack_a.length <= 0) {
       return;
     }
@@ -227,7 +288,7 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveRrb () {
+  psPrimitiveRrb() {
     if (this.state.stack_b.length <= 0) {
       return;
     }
@@ -239,14 +300,120 @@ class Visualiser extends React.Component<IVisualiserProps, IVisualiserState> {
     })
   }
 
-  psPrimitiveRrr () {
+  psPrimitiveRrr() {
     this.psPrimitiveRra();
     this.psPrimitiveRrb();
+  }
+
+  executeMove(move: Move) {
+    switch(move){
+      case Move.Sa:
+        this.psPrimitiveSa();
+        break;
+      case Move.Sb:
+        this.psPrimitiveSb();
+        break;
+      case Move.Ss:
+        this.psPrimitiveSs();
+        break;
+      case Move.Pa:
+        this.psPrimitivePa();
+        break;
+      case Move.Pb:
+        this.psPrimitivePb();
+        break;
+      case Move.Ra:
+        this.psPrimitiveRa();
+        break;
+      case Move.Rb:
+        this.psPrimitiveRb();
+        break;
+      case Move.Rr:
+        this.psPrimitiveRr();
+        break;
+      case Move.Rra:
+        this.psPrimitiveRra();
+        break;
+      case Move.Rrb:
+        this.psPrimitiveRrb();
+        break;
+      case Move.Rrr:
+        this.psPrimitiveRrr();
+        break;
+    }
+  }
+
+  executeReverseMove(move: Move, skipped: Boolean) {
+    if (skipped === true) {
+      return;
+    }
+    switch(move){
+      case Move.Sa:
+        this.psPrimitiveSa();
+        break;
+      case Move.Sb:
+        this.psPrimitiveSb();
+        break;
+      case Move.Ss:
+        this.psPrimitiveSs();
+        break;
+      case Move.Pa:
+        this.psPrimitivePb();
+        break;
+      case Move.Pb:
+        this.psPrimitivePa();
+        break;
+      case Move.Ra:
+        this.psPrimitiveRra();
+        break;
+      case Move.Rb:
+        this.psPrimitiveRrb();
+        break;
+      case Move.Rr:
+        this.psPrimitiveRrr();
+        break;
+      case Move.Rra:
+        this.psPrimitiveRa();
+        break;
+      case Move.Rrb:
+        this.psPrimitiveRb();
+        break;
+      case Move.Rrr:
+        this.psPrimitiveRr();
+        break;
+    }
+  }
+
+  stepForward() {
+    if (this.state.next_move_num < this.state.moves.length) {
+      this.executeMove(this.state.moves[this.state.next_move_num]);
+      this.setState({
+        next_move_num: this.state.next_move_num + 1,
+      });
+    }
+  }
+
+  stepBackward() {
+    if (this.state.next_move_num > 0) {
+      this.executeReverseMove(
+        this.state.moves[this.state.next_move_num - 1], 
+        this.state.skipped_moves[this.state.next_move_num - 1]
+      )
+      this.setState({
+        next_move_num: this.state.next_move_num - 1,
+      });
+    }
   }
 
   render() {
     return (
       <div className="visualiser">
+        <div className="menu">
+          <Menu
+            stepForward={() => this.stepForward()}
+            stepBackward={() => this.stepBackward()}
+          />
+        </div>
         <div className="stack_a">
           <Stack values={this.state.stack_a}/>
         </div>
